2ba1f1dddf4e47f0c409ccfa2fb6512f593e32c7
congtrinh0209 <congtrinh0209@gmail.com>
2021-10-14 15:08:43 +0700

    update

--- src/store.js
+++ src/store.js
@@ -6,10 +6,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    indexTab: 0,
   },
   getters: {
+    getIndexTab: (state) => state.indexTab
   },
   mutations: {
+    SET_INDEXTAB(state, indexTab) {
+      state.indexTab = indexTab
+    }
   },
   actions: {
     
